df18f7bf0f708e41368a2abf87378890f8eca25e
Mannarinoo <joey_mannarino@yahoo.com>
2020-02-09 15:48:14 -0500

    Hooking up heroku to gulp and mocha

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var math = require('./math');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,6 +9,13 @@ router.get('/', function(req, res, next) {
 
 router.get('/DevOps', function(req, res, next) {
   res.render('DevOps', { title: 'What is DevOps?' });
+});
+
+router.get('/api/add', function(req, res, next) {
+  var n1 = parseInt(req.query.n1);
+  var n2 = parseInt(req.query.n2);
+  res.json(math.add(n1,n2));
+
 });
 
 module.exports = router;
