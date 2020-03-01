134c55b563793a1b69a4f93d0bff7f8a7dad28e1
Mannarinoo <joey_mannarino@yahoo.com>
2020-03-01 16:49:28 -0500

    functioning winston logging

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var math = require('./math');
+var logger = require('../logger');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,10 +11,11 @@ router.get('/', function(req, res, next) {
 router.get('/DevOps', function(req, res, next) {
   res.render('DevOps', { title: 'What is DevOps?' });
 });
-
+// api add n1 & n2
 router.get('/api/add', function(req, res, next) {
   var n1 = parseInt(req.query.n1);
   var n2 = parseInt(req.query.n2);
+  logger.info('/api/add Used to add ' + n1 + ' and ' + n2);
   res.json(math.add(n1,n2));
 
 });
